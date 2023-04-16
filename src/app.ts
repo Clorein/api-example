9574aee9e98b3ff715a16d7fc2d877f2acd83249
Clorein <jose.carlos@aluno.uepb.edu.br>
2023-04-16 11:44:21 -0300

    Add server and app

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,17 @@
-console.log('Hello World')
+import express from "express";
+import { config } from "dotenv";
+import cors from 'cors'
+
+config();
+export const app = express();
+
+app.use(express.json())
+app.use(cors())
+
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Servidor executando na porta ${port}`);
+  console.log(`Acesso: http://localhost:8000`);
+});
